238eb6e05288d5680875e9aff08e8871860d0e3d
Bruno Monteiro <brunex92@gmail.com>
2017-05-24 16:14:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,14 +24,14 @@ int clockAux = 0;
 
 //Função para gerar número aleatório entre 0 e 1
 double zeroToOne(){
-    return (double)rand()/(double)RAND_MAX;
+	return (double)rand()/(double)RAND_MAX;
 }
 
 //Coluna 2 -> método da transformação inversa
 double inverseTransformation(int lambda){
-    double u = zeroToOne();
-    u = zeroToOne();
-    return (-lambda) * log(u);
+	double u = zeroToOne();
+	u = zeroToOne();
+	return (-lambda) * log(u);
 }
 
 //Coluna 4 -> método da rejeição utilizando distribuição de poisson
@@ -62,56 +62,56 @@ double rejectionMethod(int lambda){
 
 //Função para transformar o valor em string
 string to_string(double value) {
-    stringstream sstr;
-    string straux;
-    sstr << value;
-    straux = sstr.str();
+	stringstream sstr;
+	string straux;
+	sstr << value;
+	straux = sstr.str();
 
-    return straux;
+	return straux;
 }
 
 //Função para substituir os '.' em ',' (para melhor visualização na tabela)
 string replaceChar(string str, char ch1, char ch2) {
-    for (int i = 0; i < str.length(); ++i) {
-        if (str[i] == ch1)
-            str[i] = ch2;
-    }
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == ch1)
+			str[i] = ch2;
+	}
 
-  return str;
+	return str;
 }
 
 int main(){
-    srand((unsigned)time(0));
-    double tec, tecClock, serviceTime, initialTime = 0, packageTimeQueue, packageTimeSystem, freeTime, clockFinal;
+	srand((unsigned)time(0));
+	double tec, tecClock, serviceTime, initialTime = 0, packageTimeQueue, packageTimeSystem, freeTime, clockFinal;
 
-    ofstream file;
-    file.open("resultado.csv");
-    file << "Nº Pacote;Tempo desde a última chegada do pacote anterior;Tempo de chegada no relógio;Tempo de serviço;Tempo de início do roteamento;Tempo do pacote na fila do roteador;Tempo final do roteamento no relógio;Tempo do pacote no roteador;Tempo livre do servidor;\n";
+	ofstream file;
+	file.open("resultado.csv");
+	file << "Nº Pacote;Tempo desde a última chegada do pacote anterior;Tempo de chegada no relógio;Tempo de serviço;Tempo de início do roteamento;Tempo do pacote na fila do roteador;Tempo final do roteamento no relógio;Tempo do pacote no roteador;Tempo livre do servidor;\n";
 
-    for (int countPackage = 1; countPackage <= numPackages; countPackage++){
-        tec = inverseTransformation(12);        //Coluna 2 -> Tempo desde a última chegada
-        tecClock = clockBefore + tec;         //Coluna 3 -> Tempo de chegada no relógio
-        serviceTime = rejectionMethod(6);               //Coluna 4 -> Tempo de serviço
+	for (int countPackage = 1; countPackage <= numPackages; countPackage++){
+		tec = inverseTransformation(12);        //Coluna 2 -> Tempo desde a última chegada
+		tecClock = clockBefore + tec;         //Coluna 3 -> Tempo de chegada no relógio
+		serviceTime = rejectionMethod(6);               //Coluna 4 -> Tempo de serviço
 
-        //Coluna 5 -> Tempo de início do serviço no relógio
-        if (tecClock > clockAux)
-            initialTime = tecClock;
-        else
-            initialTime = clockAux;
+		//Coluna 5 -> Tempo de início do serviço no relógio
+		if (tecClock > clockAux)
+			initialTime = tecClock;
+		else
+			initialTime = clockAux;
 
-        packageTimeQueue = initialTime - tecClock;   //Coluna 6 -> Tempo do pacote na fila
-        clockFinal = initialTime + serviceTime;              //Coluna 7 -> Tempo final do serviço no relógio
-        packageTimeSystem = clockFinal - tecClock;   //Coluna 8 -> Tempo do pacote no sistema
-        freeTime = initialTime - clockAux;        //Coluna 9 -> Tempo livre do operador
+		packageTimeQueue = initialTime - tecClock;   //Coluna 6 -> Tempo do pacote na fila
+		clockFinal = initialTime + serviceTime;              //Coluna 7 -> Tempo final do serviço no relógio
+		packageTimeSystem = clockFinal - tecClock;   //Coluna 8 -> Tempo do pacote no sistema
+		freeTime = initialTime - clockAux;        //Coluna 9 -> Tempo livre do operador
 
-        cout << countPackage << " " << tec << " " << tecClock << " " << serviceTime << " " << initialTime << " " <<  packageTimeQueue << " " << clockFinal << " " << packageTimeSystem << " " << freeTime << endl;
-        file << countPackage << ";" << replaceChar(to_string(tec), '.', ',') << ";" << replaceChar(to_string(tecClock), '.', ',')  << ";"
-             << replaceChar(to_string(serviceTime), '.', ',')  << ";" << replaceChar(to_string(initialTime), '.', ',')  << ";" << replaceChar(to_string(packageTimeQueue), '.', ',')  << ";"
-             << replaceChar(to_string(clockFinal), '.', ',')  << ";" << replaceChar(to_string(packageTimeSystem), '.', ',')  << ";" << replaceChar(to_string(freeTime), '.', ',')  << ";\n";
+		cout << countPackage << " " << tec << " " << tecClock << " " << serviceTime << " " << initialTime << " " <<  packageTimeQueue << " " << clockFinal << " " << packageTimeSystem << " " << freeTime << endl;
+		file << countPackage << ";" << replaceChar(to_string(tec), '.', ',') << ";" << replaceChar(to_string(tecClock), '.', ',')  << ";"
+			 << replaceChar(to_string(serviceTime), '.', ',')  << ";" << replaceChar(to_string(initialTime), '.', ',')  << ";" << replaceChar(to_string(packageTimeQueue), '.', ',')  << ";"
+			 << replaceChar(to_string(clockFinal), '.', ',')  << ";" << replaceChar(to_string(packageTimeSystem), '.', ',')  << ";" << replaceChar(to_string(freeTime), '.', ',')  << ";\n";
 
-        clockBefore = tecClock;
-        clockAux = clockFinal;
-    }
-    file.close();
-    return 0;
+		clockBefore = tecClock;
+		clockAux = clockFinal;
+	}
+	file.close();
+	return 0;
 }
